23c2088df4a0b2652b1ae4277b304b49acff8275
LIBINLUVIS <libinluvis10@gmail.com>
2022-10-25 16:28:37 +0530

    Update db.js

--- backend apis/db.js
+++ backend apis/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 // const mongoURI = "mongodb://localhost:27017/blog"
-const mongoURI="mongodb+srv://libin123:mongodb123@blogy.ctx3wnb.mongodb.net/?retryWrites=true&w=majority"
+
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,()=>{
